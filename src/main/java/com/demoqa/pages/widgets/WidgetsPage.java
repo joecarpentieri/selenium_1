7a3392377af33eef64de26948fb93c55ed6fe55b
joecarpentieri <joecarpentieri1@gmail.com>
2026-01-02 14:06:00 +0000

    -Started following 'Working with Dates'. Created method to select date selector and created DatePickerMenuPage.java.

--- src/main/java/com/demoqa/pages/widgets/WidgetsPage.java
+++ src/main/java/com/demoqa/pages/widgets/WidgetsPage.java
@@ -8,10 +8,17 @@ import static utilities.JavaScriptUtility.scrollToElementJS;
 public class WidgetsPage extends HomePage {
 
     private By selectMenuItem = By.xpath("//li[@id='item-8']/span[text()='Select Menu']");
+    private By datePickerMenuItem = By.xpath("//li[@id='item-2']//span[text()='Date Picker']");
 
     public SelectMenuPage selectMenu() {
         scrollToElementJS(selectMenuItem);
         click(selectMenuItem);
         return new SelectMenuPage();
+    }
+
+    public DatePickerMenuPage clickDatePicker() {
+        scrollToElementJS(datePickerMenuItem);
+        click(datePickerMenuItem);
+        return new DatePickerMenuPage();
     }
 }
