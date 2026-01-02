ad83d26e3ec24cca2cef146ea4666a9fc5561739
joecarpentieri <joecarpentieri1@gmail.com>
2026-01-02 15:04:29 +0000

    - Started following 'Working with Dates'. Created method to select date selector and created DatePickerMenuPage.java

--- src/main/java/com/demoqa/pages/widgets/DatePickerMenuPage.java
+++ src/main/java/com/demoqa/pages/widgets/DatePickerMenuPage.java
@@ -1,4 +1,36 @@
 package com.demoqa.pages.widgets;
 
+import org.openqa.selenium.By;
+
+import static utilities.DropDownUtility.selectByVisibleText;
+
 public class DatePickerMenuPage extends WidgetsPage {
+
+    private By selectDateField = By.id("datePickerMonthYearInput");
+    private By monthDropDown = By.className("react-datepicker__month-select");
+    private By yearDropDown = By.className("react-datepicker__year-select");
+
+    private By dayValue(String day) { // private as By is a webdriver API - do not want test to have access to the method
+        return By.xpath("//div[contains(@class,'react-datepicker__day react-datepicker__day--')][text()='"+ day +"']");
+    }
+
+    public void clickDay(String day) {
+        click(dayValue(day));
+    }
+
+    public void clickSelect() { // clicks on the selector
+        click(selectDateField);
+    }
+
+    public String getDate() { //gets the date selected
+        return find(selectDateField).getAttribute("value");
+    }
+
+    public void selectMonth(String month) {
+        selectByVisibleText(monthDropDown, month);
+    }
+
+    public void selectYear(String year) {
+        selectByVisibleText(yearDropDown, year);
+    }
 }
